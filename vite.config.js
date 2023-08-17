6763934e9a5f6183230f8ee661494a548b0e695a
lavaxcii <lavaxcii@gmail.com>
2023-08-17 10:27:32 +0200

    Add gitub pages dependencie.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/js-pro13-reactjs-shopping-cart/',
+  build: {
+    outDir: './build'
+  }
 })
